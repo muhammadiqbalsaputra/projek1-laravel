2a9daf8b9ad1a185f720517968bce49c0266df18
muhammadiqbalsaputra <muhammad.iqbal.sap@gmail.com>
2025-04-18 20:58:09 +0700

    [Database - Menambahkan tabel Products]

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoriesFactory> */
-    protected $table = 'product_categories'; // Sesuaikan dengan nama tabel kalian }
+    protected $table = 'products'; // Sesuaikan dengan nama tabel kalian }
 }
